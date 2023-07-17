1d454dbb250f40083376d28a957dc97e475d04a2
K1rel <133015399+K1rel@users.noreply.github.com>
2023-07-18 00:53:46 +0200

    Add files via upload

---
+++ AgeCalculator/script.js
@@ -0,0 +1,16 @@
+const dobInput = document.getElementById("dob");
+const calculateBtn = document.getElementById("calculate");
+const result = document.getElementById("result");
+
+
+calculateBtn.addEventListener("click",function(){
+
+    const dob = new Date(dobInput.value);
+    const ageInMs = Date.now() - dob.getTime();
+    const ageDate = new Date(ageInMs);
+    const age = Math.abs(ageDate.getUTCFullYear() - 1970);
+
+    result.innerHTML = `Your age is ${age} years.`;
+
+
+})
